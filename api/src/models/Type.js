7a67e9546224fe1f3bc370259da6e029427ba089
Huaripaz <huaripateypaz@hotmail.com>
2022-10-23 23:23:07 -0300

    First Commit

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -4,21 +4,9 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define('type', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true,
-        },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-    }),
-    {
-        timestamps: false,
-        // sequalize crea automaticamente los modelos createAt y updateAt, esto lo cancelamos con el timestamps.
-        freezeTableName: true,
-        // nos sirve para verificar que el nombre de la tabla es igual al nombre del modelo que le estamos enviando.
-    };
+    });
 };
