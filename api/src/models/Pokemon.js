5f7052f6a7fc7fbc189068b211a4818ffce81122
Huaripaz <huaripateypaz@hotmail.com>
2022-10-23 21:57:24 -0300

    Test Commit

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,51 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
-  });
+    health: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  }),
+  {
+    timestamps: false,
+    // sequalize crea automaticamente los modelos createAt y updateAt, esto lo cancelamos con el timestamps.
+    freezeTableName: true,
+    // nos sirve para verificar que el nombre de la tabla es igual al nombre del modelo que le estamos enviando.
+  };
 };
