9bdff6370d15ab194e3b7cdcceeb461743b6d165
Huaripaz <huaripateypaz@hotmail.com>
2022-11-10 12:53:09 -0300

    Finish

--- index.js
+++ index.js
@@ -18,13 +18,11 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const allPoke = require('./src/Controllers/Pokemon/allInfo.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    allPoke();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
