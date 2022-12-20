a7e4bb2dbd97e57d2c6b78c6d6c813225f39f33c
Huaripaz <huaripateypaz@hotmail.com>
2022-12-20 19:36:32 -0300

    Landing Finish

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -2,17 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoLand from "../logo-landing.png";
 import buttonHome from "./button-home.png"
+import linkedin from './linkedin.png'
+import gitHub from './github.png'
 import "./Landing.css";
 
 export default function LandingPage() {
     return (
-        <div className='container'>
-            <div className='tittle2'>
-                <img src={logoLand} alt='logoLand' className='logoLand'></img>
+        <div className='landing'>
+            <div className='social-media'>
+                <a href='https://github.com/Huaripaz'>
+                    <img src={gitHub} alt='gitHub' className='gitHub'></img>
+                </a>
+                <a href='https://www.linkedin.com/in/huari-patey-paz-17588924a/'>
+                    <img src={linkedin} alt='linkedin' className='linkedin'></img>
+                </a>
             </div>
-            <Link to='/home'>
-                <img src={buttonHome} alt='buttonHome' className='buttonHome'></img>
-            </Link>
+            <div className='container'>
+                <div className='tittle2'>
+                    <img src={logoLand} alt='logoLand' className='logoLand'></img>
+                </div>
+                <Link to='/home'>
+                    <img src={buttonHome} alt='buttonHome' className='buttonHome'></img>
+                </Link>
+            </div>
+            <h2 className='text-footer'>
+            Huari Patey Paz © Henry's individual project 
+            </h2>
         </div>
     );
 };
